f39ab40224dc533f0d21f31804bd50f6b6592b57
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-06-17 21:58:47 +0200

    Add path

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('index/', views.index)
+	path('index/', views.index),
+	path('add_to_cart/<int: goods_id>/', views.add_to_cart),
+	path('remove_from_cart/<int: goods_id>/', views.remove_from_cart),
+	path('goods/detail/<int: goods_id>/',views.detail),
+	path('open_cart/<int: goods_id>/', views.open_cart),
+	path('order/', views.order),
+	path('categories/<int: categories_id>/', views.categories),
+	path('about/', views.about),
+	path('contact/', views.contact)
 ]
