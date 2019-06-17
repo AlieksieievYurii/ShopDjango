9347f517eaebc20989b1d2b8923c179d5c7f8f19
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-06-17 21:36:56 +0200

    Work on views

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 def index(request):
-	return render(request, 'shop/index.html')
+	goods = models.Goods.objects.order_by('?')
+	title = 'Index'
+	categories = models.Categories.objects.all()
+	count_goods_in_cart = models.Cart.objects.all().count()
+	content = {
+		'goods': goods,
+		'title': title,
+		'categories': categories,
+		'count_goods_in_cart': count_goods_in_cart
+	}
+	return render(request, 'shop/index.html', context=content)
+
+
+def add_to_cart(request):
+	pass
+
+
+def remove_from_cart(request):
+	pass
+
+
+def detail(request):
+	pass
+
+
+def open_cart(request):
+	pass
+
+
+def order(request):
+	pass
+
+
+def categories(request):
+	pass
+
+
+def about(request):
+	pass
+
+
+def contact(request):
+	pass
