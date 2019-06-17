674248e3ad7262dd8e036df896bc1ebfd15f1363
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-06-17 21:16:19 +0200

    Create models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+	name = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.name
+
+
+class Goods(models.Model):
+	name = models.CharField(max_length=20)
+	description = models.TextField()
+	price = models.IntegerField()
+	available_count = models.IntegerField()
+	img = models.CharField(max_length=50)
+	category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
+class Cart(models.Model):
+	goods = models.OneToOneField(Goods, on_delete=models.CASCADE)
+	count = models.IntegerField()
+
+	def __str__(self):
+		return '{} -> {}'.format(self.goods.name, self.count)
+
