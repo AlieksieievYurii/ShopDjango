dfe4b4f0cf674d26f15b8cf221a221c12f4dc54f
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-06-20 21:12:13 +0200

    Fix problem with define Exception

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from . import models
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 def get_products(request, categories_id=None):
